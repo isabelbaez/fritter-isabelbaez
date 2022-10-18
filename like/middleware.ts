24c5e5f894a31017ea1ddb39a80df32c6399c444
Isabel Baez <ibaez@mit.edu>
2022-10-17 23:51:42 -0400

    Update middleware.ts

--- like/middleware.ts
+++ like/middleware.ts
@@ -11,6 +11,7 @@ import LikeCollection from '../like/collection';
  const isLikeExists = async (req: Request, res: Response, next: NextFunction) => {
   const validFormat = Types.ObjectId.isValid(req.params.likeId);
   const like = validFormat ? await LikeCollection.findOne(req.params.likeId) : '';
+
   if (!like) {
     res.status(404).json({
       error: {
@@ -27,6 +28,7 @@ import LikeCollection from '../like/collection';
 const isValidLikeParent = async (req: Request, res: Response, next: NextFunction) => {
   const validFormat = Types.ObjectId.isValid(req.body.parentId);
   const freet = validFormat ? await FreetCollection.findOne(req.body.parentId) : '';
+
   if (!freet) {
     res.status(404).json({
       error: {
@@ -35,6 +37,15 @@ const isValidLikeParent = async (req: Request, res: Response, next: NextFunction
     });
     return;
   }
+
+  // TODO (When Freet implementation is complete):
+  // if (req.params.userId  in freet.likes) {
+  //   res.status(403).json({
+  //     error: {
+  //       doubleLiked: `Parent freet with freet ID ${req.body.parentId} has already been liked by ${req.params.userId}`
+  //     }
+  //   });
+  //  }
 
   next();
 };
